6740f9cad7e87626e4770d0af4a6de22329d5c70
spaetzel <william@spaetzel.com>
2009-08-06 12:03:45 -0400

    start creating solution

---
+++ PubSubHubbubNetTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PubSubHubbubNetTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Maplesoft")]
+[assembly: AssemblyProduct("PubSubHubbubNetTests")]
+[assembly: AssemblyCopyright("Copyright © Maplesoft 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM componenets.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("155277e1-dc75-4e73-be51-2862a60c524c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ readme.markdown
@@ -0,0 +1,10 @@
+PubSubHubbubNet
+===
+
+This is a .net implementation of the [PubSubHubbub spec](http://pubsubhubbub.googlecode.com/svn/trunk/pubsubhubbub-core-0.1.html).
+It contains everything needed to subscribe to Topics on other hubs as well as running your own hub.
+
+License
+---
+You are free to modify this library as you like. We only ask that you attribute this original project and push any improvements
+to the original [GitHub Project](http://github.com/spaetzel/PubSubHubbubNet)
