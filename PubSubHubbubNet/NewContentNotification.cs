f2606e3ba535255d4968d51679a204ffe9a1d7c4
spaetzel <william@spaetzel.com>
2009-08-06 13:26:33 -0400

    add newcontentnotification

---
+++ PubSubHubbubNet/NewContentNotification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace PubSubHubbubNet
+{
+    public class NewContentNotification
+    {
+
+        public NewContentNotification(HttpRequest request)
+        {
+            _mode = request.Form["hub.mode"];
+            _topic = new Uri( request.Form["hub.url"] );
+        }
+
+        private string _mode;
+
+        public string Mode
+        {
+            get { return _mode; }
+        }
+
+        private Uri _topic;
+
+        public Uri Topic
+        {
+            get { return _topic; }
+        }
+    }
+}
